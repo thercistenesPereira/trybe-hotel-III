291b8c88f33fa37e94407b3313a0d8fef601695d
Thercístenes <thercistenes1@outlook.com>
2024-01-19 22:36:35 -0300

    02. Crie e popule a tabela rooms de acordo com as verificações abaixo

--- src/02_challenge.sql
+++ src/02_challenge.sql
@@ -4,27 +4,26 @@ CREATE TABLE rooms (
   id INT AUTO_INCREMENT PRIMARY KEY,
   price DECIMAL(6,2) NOT NULL,
   available BOOLEAN DEFAULT TRUE,
-  hotel_id INT NOT NULL
+  hotel_id INT NOT NULL,
+    FOREIGN KEY (hotel_id) REFERENCES hotels (id)
 );
 
-ALTER TABLE rooms ADD FOREIGN KEY (hotel_id) REFERENCES hotels (id);
-ALTER TABLE rooms DROP COLUMN hotel_id;
-
-INSERT INTO rooms (price, available)
+INSERT INTO rooms (price, available, hotel_id)
   VALUES
-    (120.35,	0),
-    (200.00,	1),
-    (329.99,	0),
-    (379.99,	1),
-    (929.99,	0),
-    (201.62,	1),
-    (699.99,	1),
-    (500.00,	1),
-    (150.05,	1),
-    (99.99,	1),
-    (199.99,	1),
-    (399.99,	1),
-    (118.01,	1),
-    (739.99,	1),
-    (829.99,	1);
+    (120.35,	0, 1),
+    (200.00,	1, 2),
+    (329.99,	0, 1),
+    (379.99,	1, 4),
+    (929.99,	0, 1),
+    (201.62,	1, 2),
+    (699.99,	1, 3),
+    (500.00,	1, 4),
+    (150.05,	1, 3),
+    (99.99,	1, 1),
+    (199.99,	1, 2),
+    (399.99,	1, 2),
+    (118.01,	1, 4),
+    (739.99,	1, 1),
+    (829.99,	1, 1);
+
 SELECT * FROM rooms;
